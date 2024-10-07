6fa0e7593574edd1485df3ff461c6b3644f8b6fd
A01369860 <166462695+A01369860@users.noreply.github.com>
2024-10-07 16:24:18 -0600

    Avance Clases

--- clases/Class1.cs
+++ clases/Class1.cs
@@ -8,5 +8,25 @@ namespace algoritmosSimulacion.clases
 {
     public class Class1
     {
+        public double Latitud {  get; set; }
+        public double Longitud {  get; set; }
+
+        public int IdPunto {  get; set; } 
+
+        public bool Activo { get; set; }
+        public string Especie {  get; set; }
+
+        public Class1()
+        { 
+        }
+        public Class1 (Class1 class1)
+        {
+            Longitud = class1.Longitud;
+            Latitud = class1.Latitud;
+            Activo = class1.Activo;
+            Especie = class1.Especie;
+            IdPunto = class1.IdPunto;
+
+        }
     }
 }
